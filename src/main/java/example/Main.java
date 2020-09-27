465b0c7b337cf19d048e783348fecf5957c15739
Romanov <evgeny.romanov@kpigroups.com>
2020-09-27 23:06:16 +0300

    migration

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -1,5 +1,6 @@
 package example;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -7,6 +8,8 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class Main extends SpringBootServletInitializer {
     public static void main(String[] args) {
+        Flyway flyway = Flyway.configure().load();
+        flyway.migrate();
         SpringApplication.run(Main.class, args);
     }
 }
